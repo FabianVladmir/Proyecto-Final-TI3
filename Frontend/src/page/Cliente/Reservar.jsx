3c1c025286af513b926ceec55b40d3c23720fa49
Edwar Jhoel Vargas <edwarjhoel.99@gmail.com>
2023-10-20 09:45:46 -0500

    "Agregando formulario Reservar"

--- Frontend/src/page/Cliente/Reservar.jsx
+++ Frontend/src/page/Cliente/Reservar.jsx
@@ -1,20 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Reservar(props) {
-    return (
-        <div>
-            <p>Reservar</p>
-            {/* <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content text-center">
-                    <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Reservar Us</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
-                    </div>
-                </div>
-            </div> */}
+const ReservarForm = () => {
+  const [formData, setFormData] = useState({
+    equipo: '',
+    modelo: '',
+    fecha: '',
+    horaInicio: '',
+    horaFin: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes manejar el envío del formulario, por ejemplo, enviando los datos a tu servidor.
+    console.log(formData);
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-lg">
+      <h2 className="text-center text-2xl font-bold text-gray-800 mb-4">Reservar Equipo/Libro</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="equipo">
+            Nombre de Equipo:
+          </label>
+          <input
+            type="text"
+            id="equipo"
+            name="equipo"
+            value={formData.equipo}
+            onChange={handleChange}
+            className="w-full p-2 border border-gray-300 rounded"
+          />
         </div>
-    );
-}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="modelo">
+            Modelo:
+          </label>
+          <input
+            type="text"
+            id="modelo"
+            name="modelo"
+            value={formData.modelo}
+            onChange={handleChange}
+            className="w-full p-2 border border-gray-300 rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="fecha">
+            Fecha:
+          </label>
+          <input
+            type="date"
+            id="fecha"
+            name="fecha"
+            value={formData.fecha}
+            onChange={handleChange}
+            className="w-full p-2 border border-gray-300 rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="horaInicio">
+            Hora de Inicio:
+          </label>
+          <input
+            type="time"
+            id="horaInicio"
+            name="horaInicio"
+            value={formData.horaInicio}
+            onChange={handleChange}
+            className="w-full p-2 border border-gray-300 rounded"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="horaFin">
+            Hora de Fin:
+          </label>
+          <input
+            type="time"
+            id="horaFin"
+            name="horaFin"
+            value={formData.horaFin}
+            onChange={handleChange}
+            className="w-full p-2 border border-gray-300 rounded"
+          />
+        </div>
+        <div className="text-center">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+          >
+            Reservar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Reservar;
+export default ReservarForm;
