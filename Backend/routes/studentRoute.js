cccca9069e709b13390cc1f5facbe920f5c94603
FabianVladimir <fflorez16@hotmail.com>
2023-10-21 01:17:51 -0500

    Agregando middlawere a los estudiantes

--- Backend/routes/studentRoute.js
+++ Backend/routes/studentRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { signIn, profile, confirmAccount, authenticateStudent } from '../controllers/studentController.js';
+import checkAuth from '../middleware/authMiddleware.js';
+
 
 const router = express.Router();
 
 router.post("/", signIn);
-router.get("/profile", profile);
 router.get("/confirmAccount/:token", confirmAccount);
 router.post("/login", authenticateStudent);
 
-
+router.get("/profile", checkAuth, profile);
 export default router;
