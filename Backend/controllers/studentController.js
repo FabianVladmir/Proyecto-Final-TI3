cb4c2ee68234d6008b73855695f675ac1bb75378
FabianVladmir <fflorez_16@hotmail.com>
2023-10-23 20:29:47 -0500

    validando el token JWT de los estudiantes

--- Backend/controllers/studentController.js
+++ Backend/controllers/studentController.js
@@ -56,11 +56,12 @@ const confirmAccount = async (req, res) => {
 }
 
 const authenticateStudent = async (req, res) => {
+
     const {email, password} = req.body;
 
     //Search a student by email
     const student = await Student.findOne({email});
-
+    console.log(student);
     if(!student){
         const error = new Error('El estudiante no existe');
         return res.status(404).json({msg: error.message});
@@ -74,14 +75,16 @@ const authenticateStudent = async (req, res) => {
 
     //check the password
     if(await student.checkPasswordStudent(password)){
-        //authenticated student        
-        res.json({
-            _id: student._id,
-            nombre: student.nombre,
-            email: student.email,
-            token: genereateJWT(student.id)
+        //authenticated student   
+        
+        res.json({token: genereateJWT(student.id)})     
+        // res.json({
+        //     _id: student._id,
+        //     nombre: student.nombre,
+        //     email: student.email,
+        //     token: genereateJWT(student.id)
             
-        })
+        // })
         console.log("password correcto")
     }else{
         const error = new Error('El password es incorrecto');
--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -5,6 +5,7 @@ import Student from "../models/Student.js";
 const checkAuth = async (req, res, next) =>{
     let token;
     //console.log(req.headers);
+    
     if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
         try {
             token = req.headers.authorization.split(" ")[1];
