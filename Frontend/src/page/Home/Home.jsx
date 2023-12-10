a18cb37ba583cc6fcddf116f02f98f4a71275087
Edwar Jhoel Vargas <edwarjhoel.99@gmail.com>
2023-12-10 09:16:11 -0500

    Separando los estilos en otro archivo Home.csx

--- Frontend/src/page/Home/Home.jsx
+++ Frontend/src/page/Home/Home.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import Logo from '../../assets/ESCUELA-TELECOMUNICACIONES-UNSA-878x426.jpg'
-
-
-const imageStyles = {
-    width: '1200px', // Ajusta el ancho de la imagen
-    height: '400px', // Ajusta la altura de la imagen
-};
-
-const containerStyles = {
-    display: 'flex',
-    flexDirection: 'column', // Establece la dirección del contenido en columnas
-    justifyContent: 'center', // Centrar horizontalmente
-    alignItems: 'center', // Centrar horizontalmente
-};
-
-
+import styles from './styles/Home.csx';
 
 function Home(props) {
     return (
-        <div style={containerStyles}>
+        <div style={styles.container}>
             <div>
-                <img src={Logo} alt="logo" style={imageStyles} />
+                <img src={Logo} alt="logo" style={styles.image} />
             </div>
 
         </div>
