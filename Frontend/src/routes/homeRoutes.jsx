d7a67e41dec39adbcbff6e3fb93ec1797f9bb939
Edwar Jhoel Vargas <edwarjhoel.99@gmail.com>
2023-12-22 13:54:53 -0500

    Agregando componente HomeVerHorario.jsx y configurando rutas

--- Frontend/src/routes/homeRoutes.jsx
+++ Frontend/src/routes/homeRoutes.jsx
@@ -6,16 +6,17 @@ import Home from '../page/Home/Home';
 import Login from '../page/Home/HomeLogin';
 import Register from '../page/Home/HomeRegister';
 import Reset from '../page/Home/HomeReset';
+import Horario from '../page/Home/HomeVerHorario';
 function HomeRoutes() {
   return (
     <div>
       <HeaderHome />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="ver-horario" element={<Horario />} />
         <Route path="login" element={<Login />} />
         <Route path="registrar" element={<Register />} />
         <Route path="reiniciar" element={<Reset />} />
-    
         <Route
           path="*"
           element={<div><h1 className="text-center text-5xl font-bold">404 Not Found</h1></div>}
